940694b1ce36d5f75aecdcaab01b9bf4eaa2e9a0
Junaid Parkar <parkarjunaid2811@gmail.com>
2023-01-15 17:03:41 +0530

    ecommerce started

---
+++ src/codes/screens/landing.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import DealsBaner from '../components/dealsBaner'
+import Navbar from '../components/navbar'
+import TopSearch from '../components/topSearch'
+import "../css/home.css"
+
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+      <DealsBaner />
+      {/* <div className='padding'> */}
+        <TopSearch />
+      {/* </div> */}
+    </>
+  )
+}
+
+export default Landing
