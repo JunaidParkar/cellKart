816867fdf52e759103c43659280f202243ed0a4b
Junaid Parkar <parkarjunaid2811@gmail.com>
2023-01-17 01:24:36 +0530

    footer added

--- src/codes/components/greatDealsByType.jsx
+++ src/codes/components/greatDealsByType.jsx
@@ -4,11 +4,26 @@ import Cards from './cards'
 import TypeCards from './typeCards'
 
 const GreatDealsByType = () => {
+
+  // const item = document.getElementById("MAIN");
+
+  // const boxScroll = (e) =>  {
+  //   if (e.deltaY > 0){
+  //     item.scrollLeft += 100
+  //     window.scrollTo(0, window.body.scrollY)
+  //     console.log(document.body.scrollTop)
+  //   }else {
+  //     item.scrollLeft += -100
+  //   }
+  // };
+  // onWheel={boxScroll}
+
+
   return (
     <>
         <div className="topSearchMain">
             <h3>Get best deals on</h3>
-            <div className="cardSection">
+            <div className="cardSection" id='MAIN'>
                 <TypeCards />
                 <TypeCards />
                 <TypeCards />
@@ -32,6 +47,18 @@ const GreatDealsByType = () => {
                 <TypeCards />
             </div>
             <div className="cardsSection">
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
                 <Cards />
                 <Cards />
                 <Cards />
