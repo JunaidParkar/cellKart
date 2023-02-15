2afb441cd8e3bb54670da29e8aed1fe5a17b6450
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2023-02-15 21:08:53 +0530

    addProduct UI added

--- src/pages/addProduct.jsx
+++ src/pages/addProduct.jsx
@@ -1,9 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { checkAdminLogin } from '../firebase/admin/loginAdmin'
 import "../css/admin/addProducts.css"
 
 const AddProduct = () => {
+
+    const [productData, setProductData] = useState({
+        name: "",
+        date: "",
+        marketPrice: "",
+        sellingPrice: "",
+        brand: "",
+        info: "",
+        productID: ""
+    })
 
     useEffect(() => {
         const checkLoginStatus = async () => {
@@ -18,6 +28,10 @@ const AddProduct = () => {
   
       const navigate = useNavigate()
 
+      const handelTypedData = (e) => {
+        setProductData({...productData, [e.target.name]: e.target.value})
+      }
+
 
   return (
     <>
@@ -26,17 +40,54 @@ const AddProduct = () => {
                 <h3>add a product</h3>
                 <div className="productInputWrapper">
                     <label htmlFor="productName">Device name</label>
-                    <input type="text" name="productName" id="productName" required />
+                    <input type="text" name="productName" id="productName" placeholder='Enter product name' onChange={e => handelTypedData(e)} required />
                 </div>
+                <div className="productInputWrapper inline">
+                    <div className="innerProductInputWrapper">
+                        <label htmlFor="date">Choose bind date</label>
+                        <input type="date" name="date" id="date" onChange={e => handelTypedData(e)} required />
+                    </div> 
+                    <div className="innerProductInputWrapper">
+                        <label htmlFor="marketPrice">Market price</label>
+                        <input type="number" name="marketPrice" placeholder='Enter market price in numbers' id="marketPrice" onChange={e => handelTypedData(e)} required />
+                    </div> 
+                    <div className="innerProductInputWrapper">
+                        <label htmlFor="sellingPrice">Selling price</label>
+                        <input type="number" name="sellingPrice" placeholder='Enter selling price in numbers' id="sellingPrice" onChange={e => handelTypedData(e)} required />
+                    </div> 
+                    <div className="innerProductInputWrapper">
+                        <label htmlFor="brand">Brand</label>
+                        <input type="number" name="brand" placeholder='Enter brand name' id="brand" onChange={e => handelTypedData(e)} required />
+                    </div> 
+                </div>
+                <div className="productInputWrapper inline">
+                    <div className="innerProductInputWrapper">
+                        <label htmlFor="image1">Upload image 1</label>
+                        <input type="file" hidden name="image1" id="image1" required />
+                        <input type="range" disabled value="0" min="0" max="100" name="" id="" />
+                    </div> 
+                    <div className="innerProductInputWrapper">
+                        <label htmlFor="image2">Upload image 2</label>
+                        <input type="file" hidden name="image2" id="image2" required />
+                        <input type="range" disabled value="0" min="0" max="100" name="" id="" />
+                    </div> 
+                    <div className="innerProductInputWrapper">
+                        <label htmlFor="image3">Upload image 3</label>
+                        <input type="file" hidden name="image3" id="image3" required />
+                        <input type="range" disabled value="0" min="0" max="100" name="" id="" />
+                    </div> 
+                    <div className="innerProductInputWrapper">
+                        <label htmlFor="image4">Upload image 4</label>
+                        <input type="file" hidden name="image4" id="image4" required />
+                        <input type="range" disabled value="0" min="0" max="100" name="" id="" />
+                    </div> 
+                </div> 
                 <div className="productInputWrapper">
-                    <label htmlFor="bindDate">original bind date</label>
-                    <input type="text" name="bindDate" id="bindDate" required />
-                </div><div className="productInputWrapper">
-                    <label htmlFor="moreInfo">some info about product</label>
-                    <input type="text" name="moreInfo" id="moreInfo" required />
+                    <label htmlFor="moreInfo">Some info about product</label>
+                    <textarea type="text" name="moreInfo" placeholder='Enter some details in short' id="moreInfo" onChange={e => handelTypedData(e)} required rows="3" />
                 </div>
                 <div className="productSubmitWrapper">
-                    <input type="submit" value="add product" />
+                    <input type="submit" value="Add product" />
                 </div>
             </form>
         </div>
